3147ca9125a7df881204f412767692949f508b0a
nluenzer <nluenzer@sr-gmbh.de>
2024-10-27 13:31:36 +0100

    Erlöse [prüfbar] Zahlungen [prüfbar] Billbee [prüfbar] Provisionen [zwischenstand 3]

--- src/main/java/MainProvisionenGoaffpro.java
+++ src/main/java/MainProvisionenGoaffpro.java
@@ -1,26 +1,50 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MainProvisionenGoaffpro {
 
     private static String API_URL = "https://api.goaffpro.com/v1/admin/affiliates?id=%s&fields=id,avatar,honorific,date_of_birth,gender,name,first_name,last_name,email,ref_code,company_name,ref_codes,coupon,coupons,phone,website,facebook,twitter,instagram,address_1,address_2,city,state,zip,country,phone,admin_note,extra_1,extra_2,extra_3,group_id,registration_ip,personal_message,payment_method,payment_details,commission,status,last_login,total_referral_earnings,total_network_earnings,total_amount_paid,total_amount_pending,total_other_earnings,number_of_orders,tax_identification_number,login_token,signup_page,comments,tags,approved_at,blocked_at,created_at,updated_at";
+
     private static String API_KEY;
+    private static String exportPath;
 
     public static void main(String[] args) {
         loadConfig();
         try {
-            String affiliateId = "15325677";
+            // String affiliateId = "15325677";   // Kempfle
+            String affiliateId = "13637495";
             String jsonResponse = makeApiRequest(String.format(API_URL, affiliateId));
             if (jsonResponse != null) {
-                processJsonResponse(jsonResponse);
+                generateXMLFromResponse(jsonResponse);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -32,6 +56,7 @@ public class MainProvisionenGoaffpro {
             Properties prop = new Properties();
             prop.load(input);
             API_KEY = prop.getProperty("goaffproAPIKey");
+            exportPath = prop.getProperty("exportPath");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -60,65 +85,135 @@ public class MainProvisionenGoaffpro {
         }
     }
 
-    private static void processJsonResponse(String jsonResponse) throws Exception {
+    private static void generateXMLFromResponse(String jsonResponse) throws ParserConfigurationException, TransformerException, IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode rootNode = objectMapper.readTree(jsonResponse);
-        JsonNode affiliatesNode = rootNode.get("affiliates");
+        JsonNode affiliateNode = rootNode.get("affiliates");
 
-        if (affiliatesNode != null && affiliatesNode.isArray()) {
-            for (JsonNode affiliate : affiliatesNode) {
-                String id = affiliate.get("id").asText();
-                String name = affiliate.get("name").asText();
-                String firstName = affiliate.get("first_name").asText();
-                String lastName = affiliate.get("last_name").asText();
-                String email = affiliate.get("email").asText();
-                String refCode = affiliate.get("ref_code").asText();
-                JsonNode refCodesNode = affiliate.get("ref_codes");
-                String refCodes = refCodesNode.isArray() ? refCodesNode.toString() : "";
-                JsonNode couponNode = affiliate.get("coupon");
-                String coupon = couponNode.isObject() ? couponNode.toString() : "";
-                JsonNode couponsNode = affiliate.get("coupons");
-                String coupons = couponsNode.isArray() ? couponsNode.toString() : "";
-                String phone = affiliate.get("phone").asText();
-                String address1 = affiliate.get("address_1").asText();
-                String city = affiliate.get("city").asText();
-                String state = affiliate.get("state").asText();
-                String country = affiliate.get("country").asText();
-                String zipCode = affiliate.get("zip").asText();
-                String taxIdentificationNumber = affiliate.get("tax_identification_number").asText();
-                String personalMessage = affiliate.get("personal_message").asText();
-                String paymentMethod = affiliate.get("payment_method").asText();
-                JsonNode paymentDetailsNode = affiliate.get("payment_details");
-                String paymentDetails = paymentDetailsNode.isObject() ? paymentDetailsNode.toString() : "";
-                String paypalEmail = paymentDetailsNode.has("paypal_email") ? paymentDetailsNode.get("paypal_email").asText() : "";
-                JsonNode commissionNode = affiliate.get("commission");
-                String commissionType = commissionNode.has("type") ? commissionNode.get("type").asText() : "";
-                String commissionAmount = commissionNode.has("amount") ? commissionNode.get("amount").asText() : "";
+        if (affiliateNode != null && affiliateNode.size() > 0) {
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+            Document document = documentBuilder.newDocument();
 
-                System.out.println("Affiliate ID: " + id);
-                System.out.println("Name: " + name);
-                System.out.println("First Name: " + firstName);
-                System.out.println("Last Name: " + lastName);
-                System.out.println("Email: " + email);
-                System.out.println("Ref Code: " + refCode);
-                System.out.println("Ref Codes: " + refCodes);
-                System.out.println("Coupon: " + coupon);
-                System.out.println("Coupons: " + coupons);
-                System.out.println("Phone: " + phone);
-                System.out.println("Address 1: " + address1);
-                System.out.println("City: " + city);
-                System.out.println("State: " + state);
-                System.out.println("Country: " + country);
-                System.out.println("ZIP Code: " + zipCode);
-                System.out.println("Tax Identification Number: " + taxIdentificationNumber);
-                System.out.println("Personal Message: " + personalMessage);
-                System.out.println("Payment Method: " + paymentMethod);
-                System.out.println("Payment Details: " + paymentDetails);
-                System.out.println("PayPal Email: " + paypalEmail);
-                System.out.println("Commission Type: " + commissionType);
-                System.out.println("Commission Amount: " + commissionAmount);
-                System.out.println("---------------------------------------");
+            // Root element
+            Element root = document.createElement("EGeckoPersonenkonten");
+            root.setAttribute("objectgroupNr", "20");
+            root.setAttribute("datumFormat", "dd.MM.yyyy");
+            document.appendChild(root);
+
+            for (JsonNode affiliate : affiliateNode) {
+                // Personenkonto element
+                Element personenkonto = document.createElement("Personenkonto");
+                root.appendChild(personenkonto);
+
+                // Add child elements to Personenkonto
+                Element kontenart = document.createElement("kontenart");
+                kontenart.appendChild(document.createTextNode("K"));
+                personenkonto.appendChild(kontenart);
+
+                Element kontonummer = document.createElement("kontonummer");
+                kontonummer.appendChild(document.createTextNode(affiliate.get("id").asText()));
+                personenkonto.appendChild(kontonummer);
+
+                Element bezeichnung = document.createElement("bezeichnung");
+                bezeichnung.appendChild(document.createTextNode(affiliate.get("name").asText()));
+                personenkonto.appendChild(bezeichnung);
+
+                // Geschaeftspartner element
+                Element geschaeftspartner = document.createElement("Geschaeftspartner");
+                personenkonto.appendChild(geschaeftspartner);
+
+                Element nummer = document.createElement("nummer");
+                nummer.appendChild(document.createTextNode("AUTO"));
+                geschaeftspartner.appendChild(nummer);
+
+                Element kzJuristischePerson = document.createElement("kzJuristischePerson");
+                kzJuristischePerson.appendChild(document.createTextNode("n"));
+                geschaeftspartner.appendChild(kzJuristischePerson);
+
+                Element anzeigename = document.createElement("anzeigename");
+                anzeigename.appendChild(document.createTextNode(affiliate.get("name").asText() + " - " + affiliate.get("email").asText()));
+                geschaeftspartner.appendChild(anzeigename);
+
+                // Personendaten element
+                Element personendaten = document.createElement("Personendaten");
+                geschaeftspartner.appendChild(personendaten);
+
+                Element gueltigVon = document.createElement("gueltigVon");
+                gueltigVon.appendChild(document.createTextNode("01.01.1900"));
+                personendaten.appendChild(gueltigVon);
+
+                Element name1 = document.createElement("name1");
+                name1.appendChild(document.createTextNode(affiliate.get("first_name").asText()));
+                personendaten.appendChild(name1);
+
+                Element name2 = document.createElement("name2");
+                name2.appendChild(document.createTextNode(affiliate.get("last_name").asText()));
+                personendaten.appendChild(name2);
+
+                // Anschrift element
+                Element anschrift = document.createElement("Anschrift");
+                geschaeftspartner.appendChild(anschrift);
+
+                Element anschriftGueltigVon = document.createElement("gueltigVon");
+                anschriftGueltigVon.appendChild(document.createTextNode("01.01.1900"));
+                anschrift.appendChild(anschriftGueltigVon);
+
+                Element anschriftName1 = document.createElement("name1");
+                anschriftName1.appendChild(document.createTextNode(affiliate.get("first_name").asText()));
+                anschrift.appendChild(anschriftName1);
+
+                Element anschriftName2 = document.createElement("name2");
+                anschriftName2.appendChild(document.createTextNode(affiliate.get("last_name").asText()));
+                anschrift.appendChild(anschriftName2);
+
+                // Extract house number from address using regex
+                String address = affiliate.get("address_1").asText();
+                String hausnummerValue = "";
+                Pattern pattern = Pattern.compile("(\\d+\\s?[a-zA-Z]*)$");
+                Matcher matcher = pattern.matcher(address);
+                if (matcher.find()) {
+                    hausnummerValue = matcher.group(1);
+                    address = address.replace(hausnummerValue, "").trim();
+                }
+
+                Element strasse = document.createElement("strasse");
+                strasse.appendChild(document.createTextNode(address));
+                anschrift.appendChild(strasse);
+
+                Element hausnummer = document.createElement("hausnummer");
+                hausnummer.appendChild(document.createTextNode(hausnummerValue));
+                anschrift.appendChild(hausnummer);
+
+                Element plz = document.createElement("plz");
+                plz.appendChild(document.createTextNode(affiliate.get("zip").asText()));
+                anschrift.appendChild(plz);
+
+                Element ort = document.createElement("ort");
+                ort.appendChild(document.createTextNode(affiliate.get("city").asText()));
+                anschrift.appendChild(ort);
+
+                Element landkennzeichen = document.createElement("landkennzeichen");
+                landkennzeichen.appendChild(document.createTextNode(affiliate.get("country").asText()));
+                anschrift.appendChild(landkennzeichen);
             }
+
+            // Write the content into an XML file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String fileName = "egecko_personenkonten_" + timestamp + ".xml";
+            Path exportFilePath = Paths.get(exportPath, fileName);
+
+            try (FileOutputStream fos = new FileOutputStream(exportFilePath.toFile())) {
+                DOMSource domSource = new DOMSource(document);
+                StreamResult streamResult = new StreamResult(fos);
+                transformer.transform(domSource, streamResult);
+            }
+
+            System.out.println("XML file generated and saved successfully: " + exportFilePath);
         }
     }
 }
